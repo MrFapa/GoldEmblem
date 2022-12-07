1850fdc8a6f2c0a46a637f83f04e383c837d31d7
R3dM1n3r <lukasmoll.3@gmail.com>
2022-12-07 20:17:53 +0100

    TileUpdate

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TileTypes;
+using TileTopping;
+    public class Tile  {
+        Vector2 position;
+        TileType type;
+        TileToppingType topping;
+        bool isWalkable;
 
-public class Tile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public Vector2 Position { get => position; set => position = value; }
+        public TileType Type { get => type; set => type = value; }
+        public TileToppingType Topping { get => topping; set => topping = value; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public Tile(Vector2 position, TileType type, TileToppingType topping, bool isWalkable) {
+            this.isWalkable = (type == TileType.land || type == TileType.coast && topping == TileToppingType.nothing) ? true : false;
+        }
+
     }
-}
--- Assets/Scripts/TileTypes.cs
+++ Assets/Scripts/TileTypes.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TileTopping
+namespace TileTypes
 {
 
     public enum TileType
